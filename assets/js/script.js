5afce096fef1046eb97afb206602d31ad2d52bb3
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2022-08-12 16:25:14 -0700

    search history and search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
+var HISTORY_KEY = "colors";
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 var searchEl = document.getElementById("search")
 var inputEl = document.getElementById("input")
+var ulEl = document.getElementById("history")
+
+var inputs = JSON.parse(localStorage.getItem(HISTORY_KEY)) ?? [];
 
 searchEl.addEventListener("click", function () {
-    console.log(inputEl.value)
+    var input = inputEl.value;
+    console.log(input)
+    if (input === "" || !states.includes(input)) {
+        return;
+    } else {
+        inputEl.value = "";
+    }
+    inputs.push(input);
+    localStorage.setItem(HISTORY_KEY, JSON.stringify(inputs));
+    renderSearchHistory()
 })
 
-for (var i = 0; i <= localStorage.length; i++) {
+var renderSearchHistory = function () {
+    ulEl.innerHTML = ""
+    for (let i = 0; i < inputs.length; i += 1) {
+        let input = inputs[i];
+        let liEl = document.createElement("li");
+        liEl.textContent = input;
+        liEl.addEventListener("click", function () {
+            console.log(i, input);
+        });
+        ulEl.appendChild(liEl);
+    }
+};
 
-}
+renderSearchHistory();
