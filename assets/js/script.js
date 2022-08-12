c7b70d14dd7b7c6a52ccaa832a39b5ff94ef4b17
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2022-08-11 19:52:16 -0700

    search input

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var searchEl = document.getElementById("search")
+var inputEl = document.getElementById("input")
+
+searchEl.addEventListener("click", function () {
+    console.log(inputEl.value)
+})
